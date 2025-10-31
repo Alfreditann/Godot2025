f4c8591357ba6b98e5df2c2a78ed3d976816c044
Alfreditann <alula006@osloskolen.no>
2025-10-31 12:51:32 +0100

    kan nå ikke skyte box men treboks kan ødelegges

--- godot-2025/bullet.gd
+++ godot-2025/bullet.gd
@@ -2,9 +2,10 @@ extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
-var speed = 800
+var speed = 1000
 func _ready():
   look_at(get_global_mouse_position())
+
 
 func _physics_process(delta):
   linear_velocity = Vector2.RIGHT.rotated(rotation) * speed
--- godot-2025/tank.gd
+++ godot-2025/tank.gd
@@ -4,13 +4,13 @@ var speed = 200
 func _physics_process(delta: float) -> void:
 	var move_direction = Vector2.ZERO
 
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_rigth"):
 		move_direction.x = 1
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("move_left"):
 		move_direction.x = -1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		move_direction.y = 1
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("move_up"):
 		move_direction.y = -1
 
 	
